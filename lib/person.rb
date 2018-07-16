bae96bf68298c178bfb4bc907af2838baa98af2e
Michael Morris <mdam2345@gmail.com>
2018-07-16 17:51:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,9 +3,10 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(attributes)
-  attributes.each do |key, value|
-    self.send(("#{key}="), value)
-end
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end
   #your code here
 end
